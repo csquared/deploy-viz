f0bfe2eab8107f2f31eca47762af256298061f07
Chris Continanza <christopher.continanza@gmail.com>
2013-01-16 21:46:47 -0800

    fix connection namespace

--- firehose/connection.rb
+++ firehose/connection.rb
@@ -1,52 +1,50 @@
-module Atlas
-  class Firehose
-    class Connection
-      attr_reader :source
+class Firehose
+  class Connection
+    attr_reader :source
 
-      def initialize(source, opts={})
-        @key    = opts[:key]
-        @secret = opts[:secret]
-        @source = source
-      end
+    def initialize(source, opts={})
+      @key    = opts[:key]
+      @secret = opts[:secret]
+      @source = source
+    end
 
-      # Pusher configuration options
-      def options
-        { encrypted: true,
-          secret: @secret }
-      end
+    # Pusher configuration options
+    def options
+      { encrypted: true,
+        secret: @secret }
+    end
 
-      def connect
-        subscribe_to_channels
-        set_callbacks
-        connect_socket
-      end
+    def connect
+      subscribe_to_channels
+      set_callbacks
+      connect_socket
+    end
 
-      # Return a Pusher socket to our stream
-      def socket
-        @socket ||= PusherClient::Socket.new(@key, options)
-      end
+    # Return a Pusher socket to our stream
+    def socket
+      @socket ||= PusherClient::Socket.new(@key, options)
+    end
 
-      # Subscribe to all channels defined on the source
-      def subscribe_to_channels
-        channels.each do |channel|
-          socket.subscribe(channel)
-        end
+    # Subscribe to all channels defined on the source
+    def subscribe_to_channels
+      channels.each do |channel|
+        socket.subscribe(channel)
       end
+    end
 
-      # Returns the channels a given source provides
-      def channels
-        source.channels
-      end
+    # Returns the channels a given source provides
+    def channels
+      source.channels
+    end
 
-      # Bind to the events that source provides
-      def set_callbacks
-        source.callback.call
-      end
+    # Bind to the events that source provides
+    def set_callbacks
+      source.callback.call
+    end
 
-      # Open the connection on the Pusher socket
-      def connect_socket
-        socket.connect
-      end
+    # Open the connection on the Pusher socket
+    def connect_socket
+      socket.connect
     end
   end
 end
