c3d473823ceff0fb9b66217cb86c2363d4f1bbe7
Chris Continanza <christopher.continanza@gmail.com>
2013-02-10 01:18:20 -0800

    redistogo

--- deploys.rb
+++ deploys.rb
@@ -1,9 +1,17 @@
 require 'redis'
 
+
+if ENV['REDISTOGO_URL']
+  uri = URI.parse(ENV['REDISTOGO_URL'])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  REDIS = Redis.new
+end
+
 class Deploys
 
   def self.redis
-    @redis ||= Redis.new
+    @redis ||= REDIS
   end
 
   def self.add(app_name)
